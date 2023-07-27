507f98603de85ba7c531fcfe309fb8d25841884a
hoangbros03 <hoangtb203@gmail.com>
2023-07-27 10:29:36 +0000

    add arg epo

--- train.py
+++ train.py
@@ -122,6 +122,7 @@ if __name__ == '__main__':
     parser.add_argument('-net', type=str, required=True, help='net type')
     parser.add_argument('-gpu', action='store_true', default=False, help='use gpu or not')
     parser.add_argument('-b', type=int, default=128, help='batch size for dataloader')
+    parser.add_argument('-epo', type=int, default=200, help='epochs')
     parser.add_argument('-warm', type=int, default=1, help='warm up training phase')
     parser.add_argument('-lr', type=float, default=0.1, help='initial learning rate')
     parser.add_argument('-resume', action='store_true', default=False, help='resume training')
@@ -201,7 +202,7 @@ if __name__ == '__main__':
         resume_epoch = last_epoch(os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder))
 
 
-    for epoch in range(1, settings.EPOCH + 1):
+    for epoch in range(1, args.epo + 1):
         if epoch > args.warm:
             train_scheduler.step(epoch)
 
